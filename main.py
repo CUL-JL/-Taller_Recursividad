b557b54a880b0103d5ad06fde724f8e2f5b927f2
Jhosua Lascarro <jdlascarroa24@ul.edu.co>
2024-09-21 00:14:57 -0500

    Up comments
    Me faltaron las mayúsculas en los comentarios.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from modules.exercise import ejecutar_opcion # importar función ejecutar_opcion
+from modules.exercise import ejecutar_opcion # Importar función ejecutar_opcion
 
-while True: # bucle 
+while True: # Bucle 
     try: ejecutar_opcion(int(input('Ejercicios:\n 1. Contador recursivo\n 2. Suma recursiva\n 3. Número par o impar\n 4. Invertir cadena\n 5. Potencia de un número\n 6. Salir\nIngrese una opción: ')))
 
-    except ValueError: # manejo de errores
+    except ValueError: # Manejo de errores
         print('Error: ingreso de valor no numérico.\n')
